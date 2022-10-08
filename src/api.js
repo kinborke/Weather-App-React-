b522b9640e8b37d086dae0a67147b1217fddd16d
Kinbor <alierdemdemir@outlook.com>
2022-10-08 10:31:56 +0300

    initial commit

---
+++ src/api.js
@@ -0,0 +1,13 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "3acb9091cdmsh0ae1fa83f45d73dp1503bfjsn45a1c1645746",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+
+export const WEATHER_API_KEY = "da906fa89608232ca221527a290b3f71";
